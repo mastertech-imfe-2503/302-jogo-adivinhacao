7616bdd3503b45ecdfb8d67740770212c7014e31
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-04-09 19:12:29 -0300

    Add instructions and make separate files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-const getRandom = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
+/* 
+1 - Gerar um número aleatório entre 0 e 50;
+2 - Pedir para o usuário chutar um número;
+3 - Avaliar a resposta do usuário;
+    3.1 - Se for igual ao número gerado, ele ganha;
+    3.2 - Se for maior que o número gerado, ele deve ser informado que seu palpite tem que ser menor;
+    3.3 - Se for menor que número gerado, ele deve ser informado que seu palpite tem que ser maior;
+    3.4 - Cada erro desconta uma vida do usuário. Ele tem 10 vidas para ganhar o jogo;
+
+PS.: As saídas e entradas tem que ser feitas no HTML
+*/
